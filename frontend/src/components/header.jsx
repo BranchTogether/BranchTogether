d907b736eaaf89c51cab1a314b2131186794be49
Daniel Jazowski <danieljjazowski@lewisu.edu>
2024-12-29 21:47:38 -0600

    added components

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Social Media Home Page</h1>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#profile">Profile</a></li>
+          <li><a href="#settings">Settings</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ frontend/src/components/userProfile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function UserProfile() {
+  return (
+    <aside className="user-profile">
+      <img src="path/to/profile-pic.jpg" alt="Profile" />
+      <h2>User Name</h2>
+      <p>Bio: Lorem ipsum dolor sit amet.</p>
+      <button>Edit Profile</button>
+    </aside>
+  );
+}
+
+export default UserProfile;
